db4f3cb5739553f0d1232e36cf394ff4900afe68
zima <xiaopihailaotou@gmail.com>
2020-01-20 15:38:18 +0800

    修改页面样式

--- js/header.js
+++ js/header.js
@@ -2,6 +2,7 @@ document.writeln("<div class=\"navbar-header\">");
 document.writeln("	<div class=\"logo\">");
 document.writeln("		<a href=\"index.html\"> <div>&nbsp;<\/div><\/a>");
 document.writeln("	<\/div>");
+document.writeln("<div class=\"logo-bottom-bordered\"><\/div>")
 document.writeln("<\/div>");
 document.writeln("<div id=\"main-nav\" class=\"navbar-collapse collapse\">");
 document.writeln("<\/div>");
